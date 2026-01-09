3b6ba010bbea0a7b27e9a0355f866ed4c541edb5
emerkins <37460398+emerkins@users.noreply.github.com>
2026-01-09 08:26:11 -0800

    Create ghg.py

---
+++ ghg.py
@@ -0,0 +1,18 @@
+from typing import *
+from dataclasses import dataclass
+import unittest
+import math
+import sys
+sys.setrecursionlimit(10**6)
+
+# Your data definitions and functions go here.
+class Tests(unittest.TestCase):
+  # Put your test cases in here.
+  pass
+
+# Remember from Lab 1: this if statements checks
+# whether this module (ghg.py) is the module
+# being executed or whether it's just being
+# imported from some other module.
+if (__name__ == '__main__'):
+  unittest.main()
