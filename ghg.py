077a84aa469b85e9e548141ad86ed5a4c5547dd7
Ethan <ejow@calpoly.edu>
2026-01-12 10:21:21 -0800

    done with the first one

--- ghg.py
+++ ghg.py
@@ -33,10 +33,24 @@ example_region_conditions : List[RegionCondition] = [RegionCondition( Region( Gl
   RegionCondition( Region( GlobeRect ( -10, 5,  150, 160), "Western Pacific Ocean", "Ocean"), 2020, 9700000, 12000000),
   RegionCondition( Region( GlobeRect ( 34.8, 36,  -122, -119.5), "Cal Poly SLO", "Mountains"), 2020, 282000, 1000000)]
 
+#Header:
+#returns a float representing Co2 emmisions from arr
+def emmisions_per_capita(place : RegionCondition) -> float:
+  try:
+    if place.pop !=0:
+      return place.ghg_rate/place.pop
+  except ValueError:
+    print(f"Population is must be greater than 0.")
+    return 0
+
+
 class Tests(unittest.TestCase):
   # Put your test cases in here.
-  pass
-
+  def test_emmisions_per_cap(self):
+    self.assertAlmostEqual(emmisions_per_capita(RegionCondition( Region( GlobeRect ( 34.8, 36,  -122, -119.5), "Cal Poly SLO", "Mountains"), 2020, 282000, 1000000)), 3.54609929078, delta=1e-9)
+    self.assertAlmostEqual(emmisions_per_capita(RegionCondition( Region( GlobeRect ( -10, 5,  150, 160), "Western Pacific Ocean", "Ocean"), 2020, 9700000, 12000000)), 1.237113402, delta=1e-9)
+    self.assertEqual(emmisions_per_capita(RegionCondition( Region( GlobeRect ( 33.60, 34.35,  -118.90, -117.65), "Los Angeles", "Other"), 2021, 0, 26900000)), None)
+    self.assertAlmostEqual(emmisions_per_capita(RegionCondition( Region( GlobeRect ( 40.45, 41.05,  -74.45, -73.55), "New York City", "Other"), 2022, 19000000, 150000000)), 7.894736842, delta=1e-9)
 # Remember from Lab 1: this if statements checks
 # whether this module (ghg.py) is the module
 # being executed or whether it's just being
