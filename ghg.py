4f2623046315ee3a6832bd4bb92ffdae85f5a8cf
Empress <empressming06@gmail.com>
2026-01-14 21:50:46 -0800

    densest done

--- ghg.py
+++ ghg.py
@@ -33,8 +33,7 @@ example_region_conditions : List[RegionCondition] = [RegionCondition( Region( Gl
   RegionCondition( Region( GlobeRect ( -10, 5,  150, 160), "Western Pacific Ocean", "Ocean"), 2020, 9700000, 12000000),
   RegionCondition( Region( GlobeRect ( 34.8, 36,  -122, -119.5), "Cal Poly SLO", "Mountains"), 2020, 282000, 1000000)]
 
-#Header:
-#returns a float representing Co2 emmisions from arr
+#returns the Co2 emmisions per person for a certain region
 def emmisions_per_capita(place : RegionCondition) -> float:
   try:
     if place.pop !=0:
@@ -43,8 +42,7 @@ def emmisions_per_capita(place : RegionCondition) -> float:
     print(f"Population is must be greater than 0.")
     return 0
 
-#header:
-#returns an float equal to the area of a GlobeRect Object.
+#returns an the area of certain rectangular region
 def area(rect : GlobeRect) -> float:
   EarthArea : float = (6371*6371*math.pi)
   width : float = abs(rect.east_long-rect.west_long)
@@ -52,12 +50,23 @@ def area(rect : GlobeRect) -> float:
   area_sect : float = heightArea*(width/360)
   return area_sect
 
-#header:
-#returns an float equal to the emmisions of a region in tons of Co2 per square kilometer.
+#returns the emmisions of a region in tons of Co2 per square kilometer
 def emissions_per_square_km(region_area : RegionCondition)-> float:
     area_of_region : float = area(region_area.region.rect)
     return region_area.ghg_rate/area_of_region
-  
+
+# Returns the name of the region with the highest density of people per square km
+def densest( regions : List[RegionCondition] ) -> str:
+  highest : float = 0
+  name : str = ""
+  for i in regions:
+    region_area : float = area( i.region.rect )
+    density : float =  i.pop/region_area
+    if density > highest:
+      highest = density
+      name = i.region.name
+  return name
+
 
 class Tests(unittest.TestCase):
   # Put your test cases in here.
@@ -66,16 +75,24 @@ class Tests(unittest.TestCase):
     self.assertAlmostEqual(emmisions_per_capita(RegionCondition( Region( GlobeRect ( -10, 5,  150, 160), "Western Pacific Ocean", "Ocean"), 2020, 9700000, 12000000)), 1.237113402, delta=1e-9)
     self.assertEqual(emmisions_per_capita(RegionCondition( Region( GlobeRect ( 33.60, 34.35,  -118.90, -117.65), "Los Angeles", "Other"), 2021, 0, 26900000)), None)
     self.assertAlmostEqual(emmisions_per_capita(RegionCondition( Region( GlobeRect ( 40.45, 41.05,  -74.45, -73.55), "New York City", "Other"), 2022, 19000000, 150000000)), 7.894736842, delta=1e-9)
+ 
   def test_area(self):
     self.assertAlmostEqual(area(GlobeRect(0.0,10.0,10.0,100.0)), 11071470.754972488, delta=1e-9)
     self.assertAlmostEqual(area(GlobeRect(0.0,90,0,90)), 63758058.98872353, delta=1e-9)
     self.assertAlmostEqual(area(GlobeRect(66.5,90,-179.9999,180)), 21152348.803840335, delta=1e-9)
     self.assertAlmostEqual(area(GlobeRect(0,1,0,180)), 2225463.118247001, delta=1e-9)
+  
   def test_emissions_per_square_km(self):
     self.assertAlmostEqual(emissions_per_square_km(example_region_conditions[0]),29655.800092,delta=1e-7)
     self.assertAlmostEqual(emissions_per_square_km(example_region_conditions[1]),2798.41544027,delta=1e-7)
     self.assertAlmostEqual(emissions_per_square_km(example_region_conditions[2]),6.49493116779,delta=1e-7)
     self.assertAlmostEqual(emissions_per_square_km(example_region_conditions[3]),33.0743284426,delta=1e-7)
+
+  def test_densest(self):
+    self.assertEqual( densest( example_region_conditions ), "New York City")
+    self.assertEqual( densest( [RegionCondition( Region( GlobeRect ( 33.60, 34.35,  -118.90, -117.65 ), "Los Angeles", "Other" ), 2021, 12990000, 26900000 ),
+      RegionCondition( Region( GlobeRect ( -10, 5,  150, 160 ), "Western Pacific Ocean", "Ocean" ), 2020, 9700000, 12000000 ) ] ), "Los Angeles" )
+
 
 # Remember from Lab 1: this if statements checks
 # whether this module (ghg.py) is the module
